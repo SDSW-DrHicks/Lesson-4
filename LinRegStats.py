bc675fd69e25e4a44bae406129518b49d0858a2c
SDSW-DrHicks <119824568+SDSW-DrHicks@users.noreply.github.com>
2023-01-30 07:52:07 -0500

    corrected

--- LinRegStats.py
+++ LinRegStats.py
@@ -2,12 +2,14 @@
 # Statistical Analysis
 #-------Basics of machine learning----------------------------
 # The basis of machine learning
-# The use of statisticl tools and analysis to generate algorithms
+# The use of statistical tools and analysis to generate algorithms
 # to predict outcomes. One basic example of this is
-# linear regression - whihc wil be covered in this lesson.
+# linear regression - which wil be covered in this lesson.
 
 # -----import numpy---------
 import numpy as np
+# for particular numpy version:
+# in terminal: pip3 install numpy==1.23.5
 import matplotlib.pyplot as py
 #----------------------------
 
@@ -50,8 +52,8 @@ py.show()
 
 #----------Linear regression------------------
 # Using a linear model, one can try to estimate,
-# while usning the values of slope and intercept,
-# what a future or preducted value would be based on
+# while using the values of slope and intercept,
+# what a future or predicted value would be based on
 # already existing data.
 
 # However, we do need to import a new package: scipy (a stats package)
@@ -68,7 +70,7 @@ y = [99,91,87,88,111,86,104,78,100,12,70,91,86]
 # this next line is a method, there I asking python to determine the:
 # -slope
 # -y-intercept
-# - r, a value whihc when squared offers a "fit" value of the regression
+# - r, a value which when squared offers a "fit" value of the regression
 # - p, a value for a null hypothesis (which assumes slope to be 0. Don't worry about it for now
 # - std_err, the standard error of the estimated slope value.
 
@@ -77,21 +79,21 @@ slope, intercept, r, p, std_err = stats.linregress(x, y)
 def linreg(x):
   return slope * x + intercept
 
-# This line of code reates a list of values for y, the output of the function and maps them.
+# This line of code creates a list of values for y, the output of the function and maps them.
 # List is the "listing of the values" while map generates an array which tabulates
-# ( think of a matrix) whihc mapes the values of (in this case) the linreg function output
+# ( think of a matrix) which maps the values of (in this case) the linreg function output
 # to the value of x, the input.
 
 model = list(map(linreg, x))
 
 py.scatter(x, y) # plot a scatter of the original data
-py.plot(x, model)# plot the linar regression model
+py.plot(x, model)# plot the linear regression model
 py.show() # show the plot with the combined data and model
 
-print(r) # pearson correlation coefficint. The sign of the value
+print(r) # pearson correlation coefficient. The sign of the value
 # is indicative of the slope of the model while the value (absolute value
 #of which is between 0 and 1) identifies how well the data fit on the line
-# the closer to 1, the better: suggesting a 1 to 1 corespondance between
+# the closer to 1, the better: suggesting a 1 to 1 correspondence between
 #model and data.
 
 #--------------Using the model-----------------------
@@ -105,5 +107,4 @@ print(value)
 value2 = linreg(15)
 print(value2)
 # the value is ~83, which is not the actual value of y when x is 15. This illustrates
-# how well the model actually fits the obeserved data.
-
+# how well the model actually fits the observed data.
